b5702987dba4271583b9e832d663482336e40800
Jens Geiregat <jens.geiregat@incubaid.com>
2012-10-18 14:54:06 +0200

    Make the opam activate script more robust

--- tools/envuser/activate.sh
+++ tools/envuser/activate.sh
@@ -1,8 +1,26 @@
 set -exu
 
-export OPAM_ROOT="${WORKSPACE}/${OPAM_ROOT}"
-export OPAM_ROOT_PATH="${WORKSPACE}/${OPAM_ROOT_PATH}"
+case ${OPAM_ROOT} in
+  /*)
+    echo "OPAM_ROOT is absolute" ;;
+  *)
+    echo "OPAM_ROOT is relative";
+    export OPAM_ROOT="${WORKSPACE}/${OPAM_ROOT}" ;;
+esac
 
-export PATH="${OPAM_ROOT}/bin:${PATH}"
+case $OPAM_ROOT_PATH in
+  /*)
+    echo "OPAM_ROOT_PATH is absolute" ;;
+  *)
+    echo "OPAM_ROOT_PATH is relative";
+    export OPAM_ROOT_PATH="${WORKSPACE}/${OPAM_ROOT_PATH}" ;;
+esac
+
+case `which opam` in
+  ${OPAM_ROOT}/bin/opam)
+    echo "PATH already patched" ;;
+  *)
+    export PATH="${OPAM_ROOT}/bin:${PATH}" ;;
+esac
+
 export OPAM="opam --root ${OPAM_ROOT_PATH}"
-
